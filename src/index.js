ab0a1de387740f37dd9fad155eda72248921fb56
IagoLast <iago.lastra@gmail.com>
2019-11-25 20:23:38 +0100

    feat: Add git filesystem

--- src/index.js
+++ src/index.js
@@ -1,40 +1,65 @@
-import * as monaco from 'monaco-editor';
 import Markdown from 'markdown-it';
-import value from './demo.txt';
-
-
+import * as pluginMark from 'markdown-it-mark';
 import * as pluginSub from 'markdown-it-sub';
 import * as pluginSup from 'markdown-it-sup';
-import * as pluginMark from 'markdown-it-mark';
+import * as monaco from 'monaco-editor';
 
 
-const parser = Markdown("default", {
-    html: true,
-    xhtmlOut: true,
-    breaks: true,
-});
+(async () => {
 
-parser.use(pluginSub);
-parser.use(pluginSup);
-parser.use(pluginMark);
+    // GIT
 
-
+    window.dir = '/tutorial-1'
+    // await pfs.rmdir(dir); // Delete old dir.
+    // await pfs.mkdir(dir);
+    // Behold - it is empty!
+    await pfs.readdir(dir);
 
 
-const preview = document.getElementById('preview');
-const editor = monaco.editor.create(document.getElementById("editor"), {
-    value,
-    language: "markdown"
-});
+    await git.clone({
+        dir,
+        corsProxy: 'https://cors.isomorphic-git.org',
+        url: 'https://github.com/iagolast/paper-app',
+        ref: 'master',
+        singleBranch: true,
+        depth: 1
+    });
 
-preview.innerHTML = parser.render(editor.getValue());
-window.MathJax.typeset();
+    const value = await pfs.readFile(`${dir}/src/demo.txt`, { encoding: 'utf8' });
 
-editor.getModel().onDidChangeContent(() => {
+
+
+
+
+
+
+
+    const parser = Markdown("default", {
+        html: true,
+        xhtmlOut: true,
+        breaks: true,
+    });
+
+    parser.use(pluginSub);
+    parser.use(pluginSup);
+    parser.use(pluginMark);
+
+
+    const preview = document.getElementById('preview');
+    const editor = monaco.editor.create(document.getElementById("editor"), {
+        value,
+        language: "markdown"
+    });
+
     preview.innerHTML = parser.render(editor.getValue());
     window.MathJax.typeset();
-});
 
-// Debounce this
-window.addEventListener("resize", editor.layout);
+    editor.getModel().onDidChangeContent(() => {
+        preview.innerHTML = parser.render(editor.getValue());
+        window.MathJax.typeset();
+    });
 
+    // Debounce this
+    window.addEventListener("resize", editor.layout);
+
+})();
